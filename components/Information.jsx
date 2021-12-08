3b432db14f6da7d07bad1c3eacc6ab99baa3f92f
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2021-12-08 01:04:21 -0300

    Nuevas mejoras

--- components/Information.jsx
+++ components/Information.jsx
@@ -1,8 +1,63 @@
 export default function Information({ Skills, information, hobbies }) {
   return (
-    <div className="row mt-1 py-2 ">
-      <div className="col-md-5 mb-2">
+    <div className="row mt-1 py-2 d-flex flex-wrap align-content-stretch justify-content-around">
+      <div className="col-sm-5 col-md-6 mb-1">
         <div className="card bg-light">
+          <div className="card-body">
+            <h4 className="card-title">Sobre mi</h4>
+            <div className="d-flex flex-wrap align-content-stretch justify-content-around">
+              <p>
+                Full stack developer jr. con capacidad de pensar nuevas ideas e
+                implementar mejoras. Soy detallista y ordenado con los proyectos
+                que me propongo
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-light mt-1">
+          <div className="card-body">
+            <h4 className="card-title">Trayectoria academica</h4>
+            <div className="d-flex flex">
+              <ul>
+                <li className="list-group-item p-1 text-black m-1 ">
+                  <h6 className="">Bachillerato</h6>
+                  <p>
+                    Colegio San Jose de La Plata
+                    <br />
+                    2005-2019
+                  </p>
+                </li>
+                <li className="list-group-item p-1 text-black m-1 ">
+                  <h6 className="">Carrera de Analista programador</h6>
+                  <p>
+                    Facultad de Informatica UNLP
+                    <br />
+                    2020 - Fecha actual
+                    <br />
+                    11 Materias aprobadas, con un promedio de 7.36
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-5  col-md-5">
+        <div className="container p-0">
+          <div className="card bg-light">
+            <div className="card-body">
+              <h4 className="card-title">Informacion</h4>
+              <ul className="list-group ">
+                {information.map((info, index) => (
+                  <li className="list-group-item p-1 text-black" key={index}>
+                    {info.Title}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-light mt-1">
           <div className="card-body">
             <h4 className="card-title">Aptitudes</h4>
             <div className="d-flex flex-wrap align-content-stretch justify-content-around">
@@ -14,22 +69,6 @@ export default function Information({ Skills, information, hobbies }) {
                   key={index}
                 />
               ))}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-7 mt-0 px-1 ">
-        <div className="container p-0">
-          <div className="card bg-light">
-            <div className="card-body">
-              <h4 className="card-title">Informacion</h4>
-              <ul className="list-group ">
-                {information.map((info, index) => (
-                  <li className="list-group-item p-1" key={index}>
-                    {info.Title}
-                  </li>
-                ))}
-              </ul>
             </div>
           </div>
         </div>
